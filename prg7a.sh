2fb01e5174d1b37d78110ad53bdf7f11bfd29c5b
Shraddhaass <98949498+Shraddhaass@users.noreply.github.com>
2022-02-24 13:49:21 +0530

    version second

---
+++ prg7a.sh
@@ -0,0 +1,20 @@
+#Write a shell script that get executed and display the message either "good morning" or "goo Afternoon" or "good night"depending upon
+# times at which user log in 
+set -- `who`
+u=$1
+t=$4
+set -- `echo $t | tr ":" " "`
+h=$1
+echo "User logged in: $h"
+if [ $h -ge 4 ] && [ $h -lt 12 ]
+then
+echo "Good Morning Mr/ms $u"
+elif [ $h -ge 12 ] && [ $h -lt 16 ]
+then
+echo "good afternoon Mr/Ms $u"
+elif [ $h -ge 16 ] && [ $h -lt 19 ]
+then
+ echo "Good Evening $u"
+ else
+ echo "good night $u"
+ fi
