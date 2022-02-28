d842b15a0cc7cc86a60f98b52640d42e96c647f0
Shraddhaass <98949498+Shraddhaass@users.noreply.github.com>
2022-02-28 16:18:46 +0530

    2a updated

--- prg2a.sh
+++ prg2a.sh
@@ -1,13 +1,9 @@
-#Write a shell script that accepts two filenames as arguments, checks if the permissions for
-#these files are identical and if the permissions are identical, output common permissions
-#otherwise output each filename followed by its permissions.
-
 echo -n "Enter file name 1:"
 read f1
 if [ -e $f1 ]
 then
 set -- `ls -ld $f1`
-file1perm=$1
+f1perm=$1
 else
 echo "file does not exit"
 exit
@@ -17,18 +13,18 @@ read f2
 if [ -e $f2 ]
 then
 set -- `ls -ld $f2`
-file2perm=$1
+f2perm=$1
 else
 echo "File does not exit"
 exit
 fi
-if [ $file1perm = $file2perm ]
+if [ $f1perm = $f2perm ]
 then
 echo "File permmision are identical"
-echo "Permission is $file1perm"
+echo "Permission is $f1perm"
 exit
 else
 echo "File permisssion are not identical"
-echo "$f1 permission is $file1perm"
-echo "$f2 permission is $file2perm"
+echo "f1 permission is $f1perm"
+echo "f2 permission is $f2perm"
 fi
