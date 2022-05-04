a3a02a7578e3da5906b894af84918392181caa75
Shraddhaass <98949498+Shraddhaass@users.noreply.github.com>
2022-05-04 19:55:54 +0530

    3b updated

---
+++ prg3b.sh
@@ -0,0 +1,6 @@
+#Write a shell script to implement terminal locking (Similar to the lock command). It should
+#prompt for the user for a password. After accepting the password entered by the user, it must
+#prompt again for the matching password as confirmation and if match occurs, it must lock the
+#keyword until a matching password is entered again by the user. Note the Script must be
+#written to disregard BREAK, control-D. No time limit need be implemented for the lock
+#duration.
