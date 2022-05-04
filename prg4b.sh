1fb1f0ebe75756ff9c6c310462fe1c86b358ad3b
Shraddhaass <98949498+Shraddhaass@users.noreply.github.com>
2022-05-04 19:56:28 +0530

    4b updated

---
+++ prg4b.sh
@@ -0,0 +1,5 @@
+#4b.Write a shell script that displays all the links to a file specified as the first argument to the
+#script. The second argument, which is optional, can be used to specify in which the search is to
+#begin. If this second argument is not present, the search is to begin in the current working
+#directory. In either case, the starting directory as well as its subdirectories at all levels must be
+#searched. The script need not include error checking.
