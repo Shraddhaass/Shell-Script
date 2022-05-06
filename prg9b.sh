ff1163bd3dcff96a030ed428cc792b5bd25513e1
Shraddhaass <98949498+Shraddhaass@users.noreply.github.com>
2022-05-06 16:43:56 +0530

    9b updated

--- prg9b.sh
+++ prg9b.sh
@@ -3,26 +3,26 @@ echo "Enter file name"
 read filename
 if [ -f $filename ]
 then
-	n= 10
+	n=40
 	while read line
 	do
 		l=`expr length "$line"`
 		if [ $l -le $n ]
 		then
 			echo $line
-			else
-			s= 1
-			e= $n
-			echo "`echo $line | cut -c $s-$e`"
-			rcl= `expr $l - $e`
+		else
+			s=1
+			e=$n
+			echo "`echo $line | cut -c $s-$e`\\"
+			rcl=`expr $l - $e`
 			while [ $rcl -ge $n ]
 			do
-				s= `expr $e + 1`
-				e= `expr $e + $n`
-				echo "`echo $line | cut -c $s - $e`"
-				rcl= `expr $l - e`
+				s=`expr $e + 1`
+				e=`expr $e + $n`
+				echo "`echo $line | cut -c $s-$e`\\"
+				rcl=`expr $l - $e`
 			done
-			s= `expr $e + 1`
+			s=`expr $e + 1`
 			echo $line | cut -c $s-
 		fi
 	done < $filename
