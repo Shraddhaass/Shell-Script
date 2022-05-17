7750de45677615c5b3fd7de53932fa25f596669d
Shraddhaass <98949498+Shraddhaass@users.noreply.github.com>
2022-05-17 16:25:32 +0530

    8b prgm

---
+++ 8b.sh
@@ -0,0 +1,7 @@
+#8b. Write a shell script that reports the logging on of as specified user within one minute after
+#he/she login. The script automatically terminates if specified user does not login during specified in period of time.
+
+pwd
+/home/mca01
+ps
+exit
