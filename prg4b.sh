5a6b4b01e0eee096bd91b90aad71bd410ad08b93
Shraddhaass <98949498+Shraddhaass@users.noreply.github.com>
2022-05-17 16:27:18 +0530

    updated

--- prg4b.sh
+++ prg4b.sh
@@ -3,3 +3,23 @@
 #begin. If this second argument is not present, the search is to begin in the current working
 #directory. In either case, the starting directory as well as its subdirectories at all levels must be
 #searched. The script need not include error checking.
+if [ $# -ge 1 ]
+then	
+	if [ $# -eq 2 ]
+	then
+		chd=$2
+	else
+		chd=`cd .`
+	fi
+	file=`find $chd -iname $1`
+	if [ `echo $file | wc -c` -gt 1 ]
+	then 
+		number=`ls -inum $file | grep -o ^[0-9]*`
+		cd $chd
+		find $chd -inum $number
+	else
+		echo No such file
+	fi
+else
+	echo Provide filename
+fi
