e4f45d5028324c52480f547140bae9c0ac0221b1
Shraddhaass <98949498+Shraddhaass@users.noreply.github.com>
2022-05-26 20:02:28 +0530

    8a updated

--- 8a.sh
+++ 8a.sh
@@ -1,13 +1,29 @@
-#8a.Write a shell script that determine the period for which as specified user is working on a
-#system and display appropriate message.
-
-
-
-
-
-h= who | grep "mca01"|tr -s " " | cut -d " " -f 4 | cut -c 1 - 2
-
-m= who | grep "mca06" | tr -s  " " | cut -d  " " -f 4 | cut -c 4-5
-
-ch= date + %H
-ch= date + %M
+if [ $# -eq 1 ]
+then 
+user=`who | grep -wc $1 `
+if [ $? -eq 0 ]
+then
+if ["$user"=`whoami` ]
+then
+currentHour=`date+ %H`
+currentMinute=`date+ m`
+currentUser=`whoami`
+set --`who | grep $currentUser`
+set --`$4 | tr ":" " "`
+loginHour=$1
+loginMinute=$2
+hours=`expr $currentHour-$loginHour`
+minutes=`expr $currentMinute-$loginMinute`
+if [ $minutes -lt 0 ]
+then
+hours=`expr 60-$loginMinute`
+minutes=`expr $loginMinute-$currentMinute`
+fi
+echo "The user `whoami` working time is $hour hours $minutes minutes"
+else
+echo "user not loggedin"
+fi
+else
+echo "user doesn't exist"
+fi
+fi
